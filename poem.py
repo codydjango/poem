b4705fb450255c9256ec5164577f4e4593c2f9bc
Cody Django <cody@powershifter.com>
2016-12-21 22:13:24 -0800

    we added some boilerplate code for generating a poem based on a dynamic syllable configuration

--- poem.py
+++ poem.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python
 
 
-def do_poem():
-    print "let's make a poem"
+class PoemGenerator:
+    # constructor method for class
+    def __init__(self, syllables):
+        self.syllables = syllables
+
+
+    def generate(self):
+        lines = []
+        for syllable in self.syllables:
+            line = self.generate_line(syllable)
+            lines.append(line)
+
+        return '\n'.join(lines)
+
+
+    def generate_line(self, syllable):
+        line = 'default line: %s' % syllable
+        return line
+
+
+# function
+def create_and_print_poem():
+    name = 'test poem'
+    poem_generator = PoemGenerator([5,7,5])    
+    poem = poem_generator.generate()
+    print poem
 
 
 if __name__ == "__main__":
-    do_poem();
+    #argparse if you want to parse command line arguments
+    create_and_print_poem();
